d4f7a7be0bf81c29f75d1d5c6dd7a44e5f1b73d9
simonwoodworth <s.woodworth@ucc.ie>
2024-10-21 16:21:42 +0100

    This is my first commit

---
+++ mymath.py
@@ -0,0 +1,14 @@
+def add(a, b, c):
+    return a+b+c
+
+def subtract(a, b, c):
+    return a-b-c
+
+def multiply(a, b, c):
+    return a*b*c
+
+def divide(a, b, c):
+    return a/b/c
+
+def power(a, b):
+    return a**b
---
+++ test_mymath.py
@@ -0,0 +1,18 @@
+from mymath import *
+
+def test_add():
+    assert add(3,4, 5) == 12
+
+def test_subtract():
+    assert subtract(3, 4, 5) == -6
+
+def test_multiply():
+    assert multiply(3, 4, 5) == 60
+
+def test_divide():
+    assert divide(3, 4, 5) == 0.15
+
+def test_power():
+    assert power(3, 2) == 9
+    assert power(2, 3) == 8
+    assert power(4, 4) == 256
