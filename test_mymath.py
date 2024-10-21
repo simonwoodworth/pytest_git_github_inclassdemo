dd806d9b8eec3bdbadbf1fc5985e8ba4dc13386f
simonwoodworth <s.woodworth@ucc.ie>
2024-10-21 16:42:10 +0100

    Added asserts for divide and mod unit tests

--- test_mymath.py
+++ test_mymath.py
@@ -1,3 +1,4 @@
+import pytest
 from mymath import *
 
 def test_add():
@@ -11,6 +12,7 @@ def test_multiply():
 
 def test_divide():
     assert divide(3, 4, 5) == 0.15
+    assert divide(4, 5, 6) == pytest.approx(0.13333, abs=0.00001)
 
 def test_power():
     assert power(3, 2) == 9
@@ -19,3 +21,5 @@ def test_power():
 
 def test_mod():
     assert mod(7, 4) == 3
+    assert mod(7, 2) == 1
+    assert mod(7, 5) == 2
